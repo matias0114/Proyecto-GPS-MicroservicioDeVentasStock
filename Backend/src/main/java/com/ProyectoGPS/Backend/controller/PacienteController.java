d34e69657256030e7cc90a9a07571e78c2d33b01
matiasjaravega <mjarav@anticipa.cl>
2025-07-13 17:14:48 -0400

    Pruea deploy

--- Backend/src/main/java/com/ProyectoGPS/Backend/controller/PacienteController.java
+++ Backend/src/main/java/com/ProyectoGPS/Backend/controller/PacienteController.java
@@ -1,14 +1,20 @@
 package com.ProyectoGPS.Backend.controller;
 
-import com.ProyectoGPS.Backend.dto.PacienteDTO;
-import com.ProyectoGPS.Backend.service.PacienteService;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;            // <— Asegúrate de esto
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import com.ProyectoGPS.Backend.dto.PacienteDTO;
+import com.ProyectoGPS.Backend.service.PacienteService;
 
 @RestController
 @RequestMapping("/api/pacientes")
